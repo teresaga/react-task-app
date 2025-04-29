d2cb10b39523f0f97bd9c6ebfc9c465d5b6df9be
Teresa <teresagalaviza@gmail.com>
2025-04-28 23:20:23 -0700

    register user api

--- src/layouts/LoginAndRegister/Register.tsx
+++ src/layouts/LoginAndRegister/Register.tsx
@@ -1,6 +1,56 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Register = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const [error, setError] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e: any) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+
+    if (formData.password !== formData.confirmPassword) {
+      setMessage('')
+      setError('Las contraseñas no son iguales.')
+      return;
+    }
+    setError('')
+
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const response = await axios.post(`${apiUrl}/api/auth/register`, formData);
+
+      if(response.status === 200) {
+        setMessage('Se ha registrado con éxito!');
+      } else {
+        const errorText = response.data;
+        setError(errorText);
+      }
+    } catch(err: any) {
+      setMessage('')
+      if (err.response && err.response.data) {
+        setError(err.response.data);
+      } else {
+        setError('Ha ocurrido un error durante el registro.');
+      }
+    }
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center flex-column flex-grow-1 bg-light">
       <div
@@ -8,10 +58,10 @@ export const Register = () => {
         style={{ width: "22rem", borderRadius: "1rem" }}
       >
         <h2 className="text-center mb-4">Registro de usuario</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Nombre de usuario
+            <label htmlFor="name" className="form-label">
+              Nombre
             </label>
             <div className="input-group">
               <span className="input-group-text">
@@ -20,8 +70,12 @@ export const Register = () => {
               <input
                 type="text"
                 className="form-control"
-                id="username"
-                placeholder="Ingresa tu nombre de usuario"
+                id="name"
+                name="name"
+                placeholder="Ingresa tu nombre"
+                value={formData.name}
+                onChange={handleChange}
+                required
               />
             </div>
           </div>
@@ -38,7 +92,10 @@ export const Register = () => {
                 type="email"
                 className="form-control"
                 id="email"
+                name="email"
                 placeholder="Ingresa tu correo electrónico"
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -55,10 +112,45 @@ export const Register = () => {
                 type="password"
                 className="form-control"
                 id="password"
-                placeholder="Crea una contraseña"
+                name="password"
+                placeholder="Ingresar contraseña"
+                value={formData.password}
+                onChange={handleChange}
               />
             </div>
           </div>
+
+          <div className="mb-3">
+            <label htmlFor="confirmPassword" className="form-label">
+              Confirmar contraseña
+            </label>
+            <div className="input-group">
+              <span className="input-group-text">
+                <i className="fas fa-lock"></i>
+              </span>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder="Confirmar contraseña"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          {error && (
+              <div className="alert alert-danger" role="alert">
+                  {error}
+              </div>
+            )}
+
+          {message && (
+              <div className="alert alert-success" role="alert">
+                  {message}
+              </div>
+            )}
 
           <button type="submit" className="btn btn-primary w-100">
             Registrarse
